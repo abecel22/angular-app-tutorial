2b579500bf782c8585cfcc31d942787b9c56e035
abecel22 <abe2211@gmail.com>
2018-06-20 02:58:04 -0500

    Added 2nd component, practiced Typescript, models, string interpolation.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
   templateUrl: '../user/user.component.html',
   styleUrls: ['../user/user.component.css']
 })
-export class UserComponent {}
+export class UserComponent implements OnInit {
+  // Properties
+  user: User;
+
+  // Methods
+  constructor() {}
+
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main',
+        city: 'Boston',
+        state: 'MA'
+      }
+    };
+  }
+}
