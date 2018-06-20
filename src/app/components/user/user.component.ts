a78b5da24ad1079dca102c53b2f542318deb6c33
abecel22 <abe2211@gmail.com>
2018-06-19 23:19:06 -0500

    Initial commit and added 1st component manually.

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: '../user/user.component.html',
+  styleUrls: ['../user/user.component.css']
+})
+export class UserComponent {}
