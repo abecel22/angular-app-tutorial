5378cc4e38c927aa003b353b65f36184774d48a5
abecel22 <abe2211@gmail.com>
2018-06-21 10:36:34 -0500

    Introduced property and class binding.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../models/User';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-users',
@@ -10,7 +11,9 @@ import { User } from '../../models/User';
 export class UsersComponent implements OnInit {
   users: User[];
   showExtended: boolean = true;
-  loaded: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = true;
+  currentClasses = {};
 
   constructor() {}
 
@@ -24,7 +27,9 @@ export class UsersComponent implements OnInit {
           street: '50 Main',
           city: 'Boston',
           state: 'MA'
-        }
+        },
+        image: 'https://farm2.staticflickr.com/1018/1430618662_fda3a30bae.jpg',
+        isActive: true
       },
       {
         firstName: 'Kevin',
@@ -34,7 +39,9 @@ export class UsersComponent implements OnInit {
           street: '20 School St',
           city: 'Lynn',
           state: 'MA'
-        }
+        },
+        image: 'https://farm6.staticflickr.com/5770/22421685249_fc8d9fab2a.jpg',
+        isActive: false
       },
       {
         firstName: 'Karen',
@@ -44,17 +51,28 @@ export class UsersComponent implements OnInit {
           street: '55 Mill St.',
           city: 'Miami',
           state: 'FL'
-        }
+        },
+        image: 'https://farm8.staticflickr.com/7503/15535273740_013487b73f.jpg',
+        isActive: true
       }
     ];
+    this.loaded = true;
+    this.setCurrentClasses();
 
-    this.addUser({
-      firstName: 'David',
-      lastName: 'Jackson'
-    });
+    // this.addUser({
+    //   firstName: 'David',
+    //   lastName: 'Jackson'
+    // });
   }
 
   addUser(user: User) {
     this.users.push(user);
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.enableAdd,
+      'big-text': this.showExtended
+    };
   }
 }
